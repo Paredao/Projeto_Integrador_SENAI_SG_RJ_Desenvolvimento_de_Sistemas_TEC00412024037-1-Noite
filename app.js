983e265e1093eb7c46d3fb591f567cf76cc17398
Senai <senai@lab-debian.senai.lan>
2025-10-21 21:01:29 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,43 +1,3 @@
-// function pesquisar() {
-//     let section = document.getElementById("resultados-pesquisa");
-//     let campoPesquisa = document.getElementById("campo-pesquisa").value
-
-//     if (!campoPesquisa) {
-//         section.innerHTML = "<p>Nada foi encontrado. Pesquise o item pelo nome ou tag.</p>"
-//         return
-//     }
-
-//     campoPesquisa = campoPesquisa.toLowerCase()
-//     let resultados = "";
-//     let nome = "";
-//     let descricao = "";
-//     let tags = "";
-
-//     for (let dado of dados) {
-//         nome = dado.nome.toLowerCase()
-//         tags = dado.descricao.toLowerCase()
-//         tags = dado.tags.toLowerCase()
-        
-//         if (nome.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-//             resultados += `
-//             <div class="item-resultado">
-//                 <h2>
-//                     <a href="#" target="_blank">${dado.nome}</a>
-//                 </h2>
-//                 <p class="descricao-meta">${dado.descricao}</p>
-//                 <a href=${dado.link} target="_blank">Mais informações</a>
-//             </div>
-//         `;
-//         }
-//     }
-
-//     if (!resultados) {
-//         resultados = "<p>Nada foi encontrado.</p>"
-//     }
-
-//     section.innerHTML = resultados;
-// }
-
 // ===== Carrossel Automático =====
 let currentSlide = 0;
 const slides = document.querySelectorAll(".banner-slide");
@@ -61,11 +21,4 @@ const mobileNav = document.querySelector(".mobile-nav");
 
 menuToggle.addEventListener("click", () => {
   mobileNav.classList.toggle("show");
-});
-
-// ===== Modo Escuro Manual =====
-const modoBtn = document.querySelector(".modo-toggle");
-modoBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  modoBtn.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
 });
