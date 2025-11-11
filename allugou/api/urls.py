d2bc8690f5449c23d1a3b7177f4d55d2278a6d68
Senai <senai@lab-debian.senai.lan>
2025-11-10 21:31:56 -0300

    Atualização.

--- allugou/api/urls.py
+++ allugou/api/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from .views import ProdutoViewSet
 
 router = routers.DefaultRouter()
-router.register(r'produtos', ProdutoViewSet)
+router.register(r'produtos', ProdutoViewSet, basename='produto')
 
 urlpatterns = router.urls
 
